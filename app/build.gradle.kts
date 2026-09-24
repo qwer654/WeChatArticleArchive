8f5e95c569ed23fc819ffa59d9be8e11f17ca347
qwer654 <34885412+qwer654@users.noreply.github.com>
2026-09-24 10:08:31 +0900

    Bump version to 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.qwer654.wechatarchive"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
     }
 
     signingConfigs {
